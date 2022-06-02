9c5a97506a396acd72476e3b2e2fdcce18eebcc4
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2022-06-01 22:34:24 -0300

    [Aula 01/06/22] Adicionando outras activitys e string.xml

---
+++ app/src/main/java/com/ds/words/LetterAdapter.kt
@@ -0,0 +1,10 @@
+package com.ds.words
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LetterAdapter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/ds/words/WordAdapter.kt
@@ -0,0 +1,10 @@
+package com.ds.words
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WordAdapter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
